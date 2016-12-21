951658b9cca5233283b82be43ba239e524aaed84
Mau Magnaguagno <maumagnaguagno@gmail.com>
2016-12-21 10:10:23 -0200

    Complete goal axioms for the Healthcare domain

--- examples/healthcare/goal-axioms.rb
+++ examples/healthcare/goal-axioms.rb
@@ -3,7 +3,7 @@
 # predicates on goals - null(g,gi,gv) suspendedG(g,gi,gv) aborted(g,gi,gv) dropped(g,gi,gv) satisfiedG(g,gi,gv)
 #=============================================
 
-# (:- (nullG ?g ?gi ?gv) (not (varG ?g ?gi ?gv)) )
+# (:- (nullG ?g ?gi ?gv) (not (varG ?g ?gi ?gv) ))
 def nullG(g, gi, gv)
   not state('varG', g, gi, gv)
 end
@@ -18,7 +18,12 @@ end
 # )
 
 def inactiveG(g, gi, gv)
-  #nullG(g, gi, gv)
+  nullG(g, gi, gv) and
+  not state('f', g, gi, gv) and
+  not state('s', g, gi, gv) and
+  not terminalG(g, gi, gv) and
+  not state('suspendedG', g, gi, gv) and
+  not activeG(g, gi, gv)
 end
 
 # Fix this
@@ -31,7 +36,11 @@ end
 # ))
 
 def activeG(g, gi, gv)
-  
+  state('activatedG', g, gi, gv) and
+  not state('f', g, gi, gv) and
+  not satisfiedG(g, gi, gv) and
+  not terminalG(g, gi, gv) and
+  not state('suspendedG', g, gi, gv)
 end
 
 # (:- (satisfiedG ?g ?gi ?gv) (and
@@ -43,7 +52,11 @@ end
 # ))
 
 def satisfiedG(g, gi, gv)
-  
+  not nullG(g, gi, gv) and
+  not terminalG(g, gi, gv) and
+  state('pg', g, gi, gv) and
+  s(g, gi, gv) and
+  not state('f', g, gi, gv)
 end
 
 # ; (:- (suspended ?g ?gi ?gv) (and (not (nullG ?g ?gi ?gv)) (not (terminal ?g ?gi ?gv)) (suspended ?g ?gi ?gv) ))
@@ -51,18 +64,18 @@ end
 # (:- (failedG ?g ?gi ?gv) (and (not (nullG ?g ?gi ?gv)) (f ?g ?gi ?gv) ))
 
 def failedG(g, gi, gv)
-  
+  not nullG(g, gi, gv) and state('f', g, gi, gv)
 end
 
 # (:- (terminatedG ?g ?gi ?gv) (and (not (nullG ?g ?gi ?gv)) (or (dropped ?g ?gi ?gv) (aborted ?g ?gi ?gv)) ))
 
 def terminatedG(g, gi, gv)
-  
+  not nullG(g, gi, gv) and (state('dropped', g, gi, gv) or state('aborted', g, gi, gv))
 end
 
 # A rule to ensure that once a goal enters a terminal state (e.g. dropped or aborted), it cannot return
 # (:- (terminalG ?g ?gi ?gv) (and (goal ?g ?gi ?a) (or (dropped ?g ?gi ?gv) (aborted ?g ?gi ?gv) ) ))
 
 def terminalG(g, gi, gv)
-  
+  @state['goal'].any? {|terms| terms[0] == g and terms[1] == gi} and (state('dropped', g, gi, gv) or state('aborted', g, gi, gv))
 end
