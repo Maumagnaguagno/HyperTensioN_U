6c38b3ec68f558a690ef0b3ac11fc6177adde9c7
Mau Magnaguagno <maumagnaguagno@gmail.com>
2016-12-21 02:22:17 -0200

    Add goal axioms

---
+++ examples/healthcare/goal-axioms.rb
@@ -0,0 +1,68 @@
+#=============================================
+# Axioms for goals
+# predicates on goals - null(g,gi,gv) suspendedG(g,gi,gv) aborted(g,gi,gv) dropped(g,gi,gv) satisfiedG(g,gi,gv)
+#=============================================
+
+# (:- (nullG ?g ?gi ?gv) (not (varG ?g ?gi ?gv)) )
+def nullG(g, gi, gv)
+  not state('varG', g, gi, gv)
+end
+
+# (:- (inactiveG ?g ?gi ?gv) (and
+#   (not (nullG ?g ?gi ?gv))
+#   (not (f ?g ?gi ?gv))
+#   (not (s ?g ?gi ?gv))
+#   (not (terminalG ?g ?gi ?gv))
+#   (not (suspendedG ?g ?gi ?gv))
+#   (not (activeG ?g ?gi ?gv)))
+# )
+
+def inactiveG(g, gi, gv)
+  #nullG(g, gi, gv)
+end
+
+# Fix this
+# (:- (activeG ?g ?gi ?gv) (and
+#   (activatedG ?g ?gi ?gv)
+#   (not (f ?g ?gi ?gv))
+#   (not (satisfiedG ?g ?gi ?gv))
+#   (not (terminalG ?g ?gi ?gv))
+#   (not (suspendedG ?g ?gi ?gv))
+# ))
+
+def activeG(g, gi, gv)
+  
+end
+
+# (:- (satisfiedG ?g ?gi ?gv) (and
+#   (not (nullG ?g ?gi ?gv))
+#   (not (terminalG ?g ?gi ?gv))
+#   (pg ?g ?gi ?gv)
+#   (s ?g ?gi ?gv)
+#   (not (f ?g ?gi ?gv))
+# ))
+
+def satisfiedG(g, gi, gv)
+  
+end
+
+# ; (:- (suspended ?g ?gi ?gv) (and (not (nullG ?g ?gi ?gv)) (not (terminal ?g ?gi ?gv)) (suspended ?g ?gi ?gv) ))
+
+# (:- (failedG ?g ?gi ?gv) (and (not (nullG ?g ?gi ?gv)) (f ?g ?gi ?gv) ))
+
+def failedG(g, gi, gv)
+  
+end
+
+# (:- (terminatedG ?g ?gi ?gv) (and (not (nullG ?g ?gi ?gv)) (or (dropped ?g ?gi ?gv) (aborted ?g ?gi ?gv)) ))
+
+def terminatedG(g, gi, gv)
+  
+end
+
+# A rule to ensure that once a goal enters a terminal state (e.g. dropped or aborted), it cannot return
+# (:- (terminalG ?g ?gi ?gv) (and (goal ?g ?gi ?a) (or (dropped ?g ?gi ?gv) (aborted ?g ?gi ?gv) ) ))
+
+def terminalG(g, gi, gv)
+  
+end
