9783279ca84c6e7e75150a8d1d137d67d6a0bd4d
Mau Magnaguagno <maumagnaguagno@gmail.com>
2016-12-21 02:22:47 -0200

    Add domain structure to Healthcare example

--- examples/healthcare/commitment-axioms.rb
+++ examples/healthcare/commitment-axioms.rb
@@ -5,17 +5,17 @@
 # pending(c,ci,cv) satisfied(c,ci,cv)
 #=============================================
 
-# (:- (null ?c ?ci ?cv) (not (var ?c ?ci ?cv)) )
+# (:- (null ?c ?ci ?cv) (not (var ?c ?ci ?cv) ))
 def null(c, ci, cv)
   not state('var', c, ci, cv)
 end
 
-# (:- (conditional ?c ?ci ?cv) (and (active ?c ?ci ?cv) (not (p ?c ?ci ?cv)) ) )
+# (:- (conditional ?c ?ci ?cv) (and (active ?c ?ci ?cv) (not (p ?c ?ci ?cv)) ))
 def conditional(c, ci, cv)
   active(c, ci, cv) and not state('p', c, ci, cv)
 end
 
-# (:- (detached ?c ?ci ?cv) (and (active ?c ?ci ?cv) (p ?c ?ci ?cv) ) )
+# (:- (detached ?c ?ci ?cv) (and (active ?c ?ci ?cv) (p ?c ?ci ?cv) ))
 def detached(c, ci, cv)
   active(c, ci, cv) and state('p', c, ci, cv)
 end
@@ -26,7 +26,7 @@ end
 #   (not (terminal ?c ?ci ?cv))
 #   (not (pending ?c ?ci ?cv))
 #   (not (satisfied ?c ?ci ?cv))
-# ) )
+# ))
 def active(c, ci, cv)
   state('var', c, ci, cv) and
   not terminal(c, ci, cv) and
@@ -34,18 +34,18 @@ def active(c, ci, cv)
   not satisfied(c, ci, cv)
 end
 
-# (:- (terminated ?c ?ci ?cv) (or (and (not (p ?c ?ci ?cv)) (cancelled ?c ?ci ?cv)) (released ?c ?ci ?cv) ) )
+# (:- (terminated ?c ?ci ?cv) (or (and (not (p ?c ?ci ?cv)) (cancelled ?c ?ci ?cv)) (released ?c ?ci ?cv) ))
 def terminated(c, ci, cv)
   ( (not state('p', c, ci, cv)) and state('cancelled', c, ci, cv) ) or state('released', c, ci, cv)
 end
 
 # ;(:- (violated ?c ?ci ?cv) (or (and (p ?c ?ci ?cv) (cancelled ?c ?ci ?cv)) (and (not (p ?c ?ci ?cv)) ) ) ) ; Previous formalization with a mistaken disjunction, detected by Pankaj
-# (:- (violated ?c ?ci ?cv) (and (p ?c ?ci ?cv) (cancelled ?c ?ci ?cv)) )
+# (:- (violated ?c ?ci ?cv) (and (p ?c ?ci ?cv) (cancelled ?c ?ci ?cv) ))
 def violated(c, ci, cv)
   state('p', c, ci, cv) and state('cancelled', c, ci, cv)
 end
 
-# (:- (satisfied ?c ?ci ?cv) (and (not (null ?c ?ci ?cv)) (not (terminal ?c ?ci ?cv)) (q ?c ?ci ?cv)) )
+# (:- (satisfied ?c ?ci ?cv) (and (not (null ?c ?ci ?cv)) (not (terminal ?c ?ci ?cv)) (q ?c ?ci ?cv) ))
 def satisfied(c, ci, cv)
   (not null(c, ci, cv)) and (not terminal(c, ci, cv)) and q(c, ci, cv)
 end
@@ -53,7 +53,7 @@ end
 # ;(:- (expired ?c ?ci ?cv) (and (not (null ?c ?ci ?cv)) (not (p ?c ?ci ?cv)) ) )
 
 # A rule to enumerate that certain states are terimnal
-# (:- (terminal ?c ?ci ?cv) (and (commitment ?c ?ci ?de ?cr) (or (cancelled ?c ?ci ?cv) (released ?c ?ci ?cv) (expired ?c ?ci ?cv))) )
+# (:- (terminal ?c ?ci ?cv) (and (commitment ?c ?ci ?de ?cr) (or (cancelled ?c ?ci ?cv) (released ?c ?ci ?cv) (expired ?c ?ci ?cv)) ))
 def terminal(c, ci, cv)
   # Free variables de and cr requires special comparison
   @state['commitment'].any? {|terms| terms[0] == c and terms[1] == ci and terms[2] == cv} and
--- examples/healthcare/healthcare.rb
+++ examples/healthcare/healthcare.rb
@@ -1,9 +1,34 @@
 require_relative 'commitment-axioms'
 require_relative 'healthcare-axioms'
 require_relative 'commitment-operators'
-#require_relative 'goal-axioms'
+require_relative 'goal-axioms'
 #require_relative 'goal-operators'
 
-def state(pre, *terms)
-  @state[pre].include?(terms)
+module Healthcare
+  include Hypertension
+  extend self
+
+  #-----------------------------------------------
+  # Domain
+  #-----------------------------------------------
+
+  @domain = {
+    # Operators
+    invisible_testSuccess => 1,
+    invisible_testFailure => 1,
+    create => 1,
+    suspend => 1,
+    reactivate => 1,
+    satisfy => 1,
+    expire => 1,
+    timeoutviolate => 1,
+    cancel => 1,
+    release => 1
+    # Methods
+    
+  }
+
+  def state(pre, *terms)
+    @state[pre].include?(terms)
+  end
 end
