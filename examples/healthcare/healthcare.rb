d214d42d13e9d5ff86cb11d3ab4a4426fcbd58c9
Mau Magnaguagno <maumagnaguagno@gmail.com>
2016-12-21 10:10:06 -0200

    Use strings in the domain structure

--- examples/healthcare/healthcare.rb
+++ examples/healthcare/healthcare.rb
@@ -15,16 +15,16 @@ module Healthcare
 
   @domain = {
     # Operators
-    invisible_testSuccess => 1,
-    invisible_testFailure => 1,
-    create => 1,
-    suspend => 1,
-    reactivate => 1,
-    satisfy => 1,
-    expire => 1,
-    timeoutviolate => 1,
-    cancel => 1,
-    release => 1
+    'invisible_testSuccess' => 1,
+    'invisible_testFailure' => 1,
+    'create' => 1,
+    'suspend' => 1,
+    'reactivate' => 1,
+    'satisfy' => 1,
+    'expire' => 1,
+    'timeoutviolate' => 1,
+    'cancel' => 1,
+    'release' => 1
     # Methods
     
   }
