1a66bd1a94531c87cf9856ba39a0e305680cc2fe
Mau Magnaguagno <maumagnaguagno@gmail.com>
2016-12-27 16:47:15 -0200

    Add problem generator to Healthcare domain

---
+++ examples/healthcare/pbgenerator.rb
@@ -0,0 +1,140 @@
+require_relative 'healthcare'
+
+# Default values
+debug = false
+min_probability = 0
+max_plans_found = 1
+patients     = 1
+physicians   = 1
+radiologists = 1
+pathologists = 1
+registrars   = 1
+hospitals    = 1
+cancers      = 1
+
+# ARGV parser
+while opt = ARGV.shift
+  case opt
+  when '-d' then debug = true
+  when '-min_prob' then min_probability = ARGV.shift.to_f
+  when '-max_plans' then max_plans_found = ARGV.shift.to_i
+  when '-patients' then patients = ARGV.shift.to_i
+  when '-physicians' then physicians = ARGV.shift.to_i
+  when '-radiologists' then radiologists = ARGV.shift.to_i
+  when '-pathologists' then pathologists = ARGV.shift.to_i
+  when '-registrars' then registrars = ARGV.shift.to_i
+  when '-hospitals' then hospitals = ARGV.shift.to_i
+  when '-cancers' then cancers = ARGV.shift.to_i
+  else raise "Unknown option: #{opt}"
+  end
+end
+
+# Objects
+PATIENT_SET     = Array.new(patients)     {|i| "patient_#{i}"}
+PHYSICIAN_SET   = Array.new(physicians)   {|i| "physician_#{i}"}
+RADIOLOGIST_SET = Array.new(radiologists) {|i| "radiologist_#{i}"}
+PATHOLOGIST_SET = Array.new(pathologists) {|i| "pathologist_#{i}"}
+REGISTRAR_SET   = Array.new(registrars)   {|i| "registrar_#{i}"}
+HOSPITAL_SET    = Array.new(hospitals)    {|i| "hospital_#{i}"}
+satisfied = 'satisfied'
+
+def commitments
+  c1 = 'C1'
+  c2 = 'C2'
+  c3 = 'C3'
+  c4 = 'C4'
+  c5 = 'C5'
+  c6 = 'C6'
+  c7 = 'C7'
+  c8 = 'C8'
+  comm = []
+  PHYSICIAN_SET.each {|physician|
+    PATIENT_SET.each {|patient|
+      comm.push(
+        [c1, c1, physician, patient],
+        [c2, c2, patient, physician],
+        [c3, c3, patient, physician]
+      )
+    }
+    RADIOLOGIST_SET.each {|radiologist|
+      comm.push(
+        [c4, c4, radiologist, physician],
+        [c5, c5, radiologist, physician]
+      )
+    }
+  }
+  RADIOLOGIST_SET.each {|radiologist|
+    PATHOLOGIST_SET.each {|pathologist|
+      comm << [c6, c6, pathologist, radiologist]
+    }
+  }
+  HOSPITAL_SET.each {|hospital|
+    PATHOLOGIST_SET.each {|pathologist|
+      comm << [c7, c7, pathologist, hospital]
+    }
+    REGISTRAR_SET.each {|registrar|
+      comm << [c8, c8, registrar, hospital]
+    }
+  }
+  comm
+end
+
+plan = Healthcare.problem(
+  # Start
+  {
+    'patient' => PATIENT_SET.map {|i| [i]},
+    'physician' => PHYSICIAN_SET.map {|i| [i]},
+    'radiologist' => RADIOLOGIST_SET.map {|i| [i]},
+    'pathologist' => PATHOLOGIST_SET.map {|i| [i]},
+    'registrar' => REGISTRAR_SET.map {|i| [i]},
+    'hospital' => HOSPITAL_SET.map {|i| [i]},
+    'patientHasCancer' => PATIENT_SET.first(cancers).map {|i| [i]},
+    'commitment' => commitments,
+    'var' => [],
+    'diagnosisRequested' => [],
+    'iAppointmentRequested' => [],
+    'iAppointmentKept' => [],
+    'imagingScan' => [],
+    'imagingRequested' => [],
+    'imagingResultsReported' => [],
+    'bAppointmentRequested' => [],
+    'bAppointmentKept' => [],
+    'biopsyReport' => [],
+    'biopsyRequested' => [],
+    'radiologyRequested' => [],
+    'treatmentPlan' => [],
+    'diagnosisProvided' => [],
+    'tissueProvided' => [],
+    'radPathResultsReported' => [],
+    'pathResultsReported' => [],
+    'patientReportedToRegistrar' => [],
+    'inRegistry' => [],
+    'TBAgreesPath' => [],
+    'TBDisagreesPath' => [],
+    'TBAgreesRad' => [],
+    'TBDisagreesRad' => [],
+    'TBAgreesPCP' => [],
+    'TBDisagreesPCP' => [],
+    'pathologyRequested' => [],
+    'escalate' => [],
+    'radRequestsAssessment' => [],
+    'phyRequestsAssessment' => [],
+    'patRequestsAssessment' => [],
+    'integratedReport' => [],
+    'reportNeedsReview' => [],
+    'cancelled' => [],
+    'released' => [],
+    'expired' => []
+  },
+  # Tasks
+  [
+    ['hospitalScenario'],
+    ['testCommitments']
+  ],
+  # Debug
+  debug,
+  # Minimal probability for plans
+  min_probability,
+  # Maximum plans found
+  max_plans_found
+)
