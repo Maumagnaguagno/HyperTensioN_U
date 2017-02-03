c2547c0d7f0ae73afd8a2ba7d3abd9b9d06f5e4c
Mau Magnaguagno <maumagnaguagno@gmail.com>
2017-02-03 15:25:06 -0200

    Add missing equality axioms for Healthcare

--- examples/healthcare/equality-axioms.rb
+++ examples/healthcare/equality-axioms.rb
@@ -7,24 +7,20 @@
 # (:- (eqGSCP ?g ?gv ?c ?cv) (and (imply (s ?g ?gi ?gv) (p ?c ?ci ?cv)) (imply (p ?c ?ci ?cv) (s ?g ?gi ?gv)) ) ) ; Basically a logical equivalence <->
 
 def eqGSCP(g, gv, c, cv)
-  s_si_unified = [G1, G2, G3, G4, G5].any? {|gi| s(g, gi, gv)}
-  p_ci_unified = [C1, C2, C3, C4, C5, C6, C7, C8, C9, C10, C11, C12].any? {|ci| p(c, ci, cv)}
-  s_si_unified == p_ci_unified
+  GOALS.any? {|gi| s(g, gi, gv)} == COMMITMENTS.any? {|ci| p(c, ci, cv)}
 end
 
-#     ;TODO make sure that the variables ?gv and ?cv get instantiated
+# ; TODO make sure that the variables ?gv and ?cv get instantiated
 # ; True of the success condition of ?g matches the consequent of ?c
 # (:- (eqGSCQ ?g ?gv ?c ?cv) (and (imply (s ?g ?gi ?gv) (q ?c ?ci ?cv)) (imply (q ?c ?ci ?cv) (s ?g ?gi ?gv)) ) ) ; Basically a logical equivalence <->
 
 def eqGSCQ(g, gv, c, cv)
-  # TODO eqGSCQ
-  raise 'eqGSCQ is not implemented'
+  GOALS.any? {|gi| s(g, gi, gv)} == COMMITMENTS.any? {|ci| q(c, ci, cv)}
 end
 
 # ; True of the the antecedent of commitment ?c1 matches the consequent of commitment ?c2 (so they are reciprocal)
 # (:- (eqCPCQ ?c1 ?cv1 ?c2 ?cv2) (and (imply (p ?c1 ?ci1 ?cv1) (q ?c2 ?ci2 ?cv2)) (imply (q ?c2 ?ci2 ?cv2) (p ?c1 ?ci1 ?cv1)) ) ) ; Basically a logical equivalence <->
 
 def eqCPCQ(c1, cv1, c2, cv2)
-  # TODO eqCPCQ
-  raise 'eqCPCQ is not implemented'
+  COMMITMENTS.any? {|ci1| p(c1, ci1, cv1)} == COMMITMENTS.any? {|ci2| q(c2, ci2, cv2)}
 end
--- examples/healthcare/healthcare.rb
+++ examples/healthcare/healthcare.rb
@@ -16,24 +16,28 @@ require_relative 'test-methods'
 require_relative 'healthcare-methods'
 require_relative 'healthcare-operators'
 
-C1 = 'C1'
-C2 = 'C2'
-C3 = 'C3'
-C4 = 'C4'
-C5 = 'C5'
-C6 = 'C6'
-C7 = 'C7'
-C8 = 'C8'
-C9 = 'C9'
-C10 = 'C10'
-C11 = 'C11'
-C12 = 'C12'
+COMMITMENTS = [
+  C1 = 'C1',
+  C2 = 'C2',
+  C3 = 'C3',
+  C4 = 'C4',
+  C5 = 'C5',
+  C6 = 'C6',
+  C7 = 'C7',
+  C8 = 'C8',
+  C9 = 'C9',
+  C10 = 'C10',
+  C11 = 'C11',
+  C12 = 'C12'
+]
 
-G1 = 'G1'
-G2 = 'G2'
-G3 = 'G3'
-G4 = 'G4'
-G5 = 'G5'
+GOALS = [
+  G1 = 'G1',
+  G2 = 'G2',
+  G3 = 'G3',
+  G4 = 'G4',
+  G5 = 'G5'
+]
 
 module Healthcare
   include Hypertension_U
