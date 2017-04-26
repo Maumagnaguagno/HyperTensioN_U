282b48fc1a1b495b8f95c0104016169ed664ab8f
Mau Magnaguagno <maumagnaguagno@gmail.com>
2017-04-26 09:33:33 -0300

    Fix and refactor Healthcare batch

--- examples/healthcare/batch.rb
+++ examples/healthcare/batch.rb
@@ -1,33 +1,32 @@
 jshop_output = ARGV.first == 'jshop'
 
+def plan(file, max_plans, min_prob, patients, n)
+  IO.write(file, `ruby pbgenerator.rb -max_plans #{max_plans} -min_prob #{min_prob} -patients #{patients} -physicians #{n} -radiologists #{n} -pathologists #{n} -registrars #{n} -hospitals #{n} -cancers #{n}`)
+end
+
 puts 'Single plan'
 1.upto(25) {|patients|
   t = Time.now.to_f
   puts "#{patients} of 25", Time.now
-  max_plans  = 1
-  min_prob   = 0
-  physicians = radiologists = pathologists = registrars = hospitals = cancers = (patients - 1) / 5 + 1
+  max_plans = 1
+  min_prob  = 0
+  n = (patients - 1) / 5 + 1
   if jshop_output
-    output = `ruby pbgenerator.rb -max_plans #{max_plans} -min_prob #{min_prob} -patients #{patients} -physicians #{physicians} -radiologists #{radiologists} -pathologists #{pathologists} -registrars #{registrars} -hospitals #{hospitals} -cancers #{cancers}`
-    IO.write("pbgenerator#{patients}_single_plan.txt", output)
+    `ruby pbgenerator.rb -max_plans #{max_plans} -min_prob #{min_prob} -patients #{patients} -physicians #{n} -radiologists #{n} -pathologists #{n} -registrars #{n} -hospitals #{n} -cancers #{n} -jshop pb#{patients}.jshop`
   else
-    `ruby pbgenerator.rb -max_plans #{max_plans} -min_prob #{min_prob} -patients #{patients} -physicians #{physicians} -radiologists #{radiologists} -pathologists #{pathologists} -registrars #{registrars} -hospitals #{hospitals} -cancers #{cancers} -jshop`
+    plan("pbgenerator#{patients}_single_plan.txt", max_plans, min_prob, patients, n)
   end
   puts "#{Time.now.to_f - t}s"
 }
+exit if jshop_output
 
 puts 'All plans'
 1.upto(25) {|patients|
   t = Time.now.to_f
   puts "#{patients} of 25", Time.now
-  max_plans  = -1
-  min_prob   = 0
-  physicians = radiologists = pathologists = registrars = hospitals = cancers = (patients - 1) / 5 + 1
-  if jshop_output
-    output = `ruby pbgenerator.rb -max_plans #{max_plans} -min_prob #{min_prob} -patients #{patients} -physicians #{physicians} -radiologists #{radiologists} -pathologists #{pathologists} -registrars #{registrars} -hospitals #{hospitals} -cancers #{cancers}`
-    IO.write("pbgenerator#{patients}_all_plans.txt", output)
-  else
-    `ruby pbgenerator.rb -max_plans #{max_plans} -min_prob #{min_prob} -patients #{patients} -physicians #{physicians} -radiologists #{radiologists} -pathologists #{pathologists} -registrars #{registrars} -hospitals #{hospitals} -cancers #{cancers}`
-  end
+  max_plans = -1
+  min_prob  = 0
+  n = (patients - 1) / 5 + 1
+  plan("pbgenerator#{patients}_all_plans.txt", max_plans, min_prob, patients, n)
   puts "#{Time.now.to_f - t}s"
 }
