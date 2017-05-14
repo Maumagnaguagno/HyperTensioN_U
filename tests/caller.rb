7d97fb917873d0e1fb44fbd241f82a0129c9f507
Mau Magnaguagno <maumagnaguagno@gmail.com>
2017-05-14 03:41:47 -0300

    Add more tests for call

--- tests/caller.rb
+++ tests/caller.rb
@@ -35,46 +35,78 @@ class Caller < Test::Unit::TestCase
     call('-1.0', ['call', '*', '-1', '1.0'])
     call('-1.0', ['call', '*', '1', '-1.0'])
     call('1.0', ['call', '*', '-1', '-1.0'])
-    call('1.0', ['call', '*', '-1', '-1.0'])
   end
 
   def test_call_div
-    
+    call('1.5', ['call', '/', '3', '2'])
+    call('1.5', ['call', '/', '3', '2.0'])
+    call('1.5', ['call', '/', '3.0', '2'])
+    call('1.5', ['call', '/', '3.0', '2.0'])
+    call('-1.0', ['call', '/', '-1', '1.0'])
+    call('-1.0', ['call', '/', '1', '-1.0'])
+    call('1.0', ['call', '/', '-1', '-1.0'])
   end
 
-  def test_call_rest
-    
+  def test_call_remainder
+    call('1.0', ['call', '%', '3', '2'])
+    call('1.0', ['call', '%', '3', '2.0'])
+    call('1.0', ['call', '%', '3.0', '2'])
+    call('1.0', ['call', '%', '3.0', '2.0'])
+    call('-0.0', ['call', '%', '-1', '1.0'])
+    call('0.0', ['call', '%', '1', '-1.0'])
+    call('-0.0', ['call', '%', '-1', '-1.0'])
   end
 
   def test_call_exp
-    
+    call('9.0', ['call', '**', '3', '2'])
+    call('9.0', ['call', '**', '3', '2.0'])
+    call('9.0', ['call', '**', '3.0', '2'])
+    call('9.0', ['call', '**', '3.0', '2.0'])
+    call('-1.0', ['call', '**', '-1', '1.0'])
+    call('1.0', ['call', '**', '1', '-1.0'])
+    call('-1.0', ['call', '**', '-1', '-1.0'])
   end
 
-  def test_call_negative
-    
+  def test_abs_negative
+    call('1.0', ['call', 'abs', '1'])
+    call('1.0', ['call', 'abs', '1.0'])
+    call('1.0', ['call', 'abs', '-1'])
+    call('1.0', ['call', 'abs', '-1.0'])
   end
 
   def test_call_sin
-    
+    # TODO
   end
 
   def test_call_cos
-    
+    # TODO
   end
 
   def test_call_tan
-    
+    # TODO
   end
 
   def test_call_equal
-    
+    # TODO
+    #call('true', ['call', '=', '1', '1'])
+    #call('true', ['call', '=', '?a', '?a'])
+    #call('false', ['call', '=', '1', '2'])
+    #call('false', ['call', '=', '2', '1'])
   end
 
   def test_call_diff
-    
+    # TODO
+    #call('false', ['call', '!=', '1', '1'])
+    #call('false', ['call', '!=', '?a', '?a'])
+    #call('true', ['call', '!=', '1', '2'])
+    #call('true', ['call', '!=', '2', '1'])
   end
 
   def test_call_compare
-    
+    # TODO
+    #call('0', ['call', '=', '1', '1'])
+    #call('0', ['call', '=', '?a', '?a'])
+    #call('-1', ['call', '=', '1', '2'])
+    #call('1', ['call', '=', '2', '1'])
   end
 end
