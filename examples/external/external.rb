d3ce5df1429516b4c85a05fabcdbaffda6a3858c
Mau Magnaguagno <maumagnaguagno@gmail.com>
2017-05-29 05:09:43 -0300

    Force external numeric regex match entire strings

--- examples/external/external.rb
+++ examples/external/external.rb
@@ -14,7 +14,7 @@ module External
   def approx(terms)
     External.state[terms.shift].any? {|terms2|
       terms == terms2 or terms.zip(terms2).all? {|t1,t2|
-        t1 == t2 or (t1 =~ /-?\d+(?>\.\d+)?/ and t2 =~ /-?\d+(?>\.\d+)?/ and (diff = ((t1 = t1.to_f) - (t2 = t2.to_f)).abs) <= 0.001 || diff / (t1 > t2 ? t1 : t2).abs <= 0.001)
+        t1 == t2 or (t1 =~ /^-?\d+(?>\.\d+)?$/ and t2 =~ /^-?\d+(?>\.\d+)?$/ and (diff = ((t1 = t1.to_f) - (t2 = t2.to_f)).abs) <= 0.001 || diff / (t1 > t2 ? t1 : t2).abs <= 0.001)
       }
     }
   end
