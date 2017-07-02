468a615de9f296fb618e44f69b3c35a7c21080e8
Mau Magnaguagno <maumagnaguagno@gmail.com>
2017-07-02 14:06:26 -0300

    Fix methods without preconditions

--- UHyper_Compiler.rb
+++ UHyper_Compiler.rb
@@ -155,7 +155,7 @@ module UHyper_Compiler
         define_methods << "\n  def #{met.first}_#{dec.first}#{variables}"
         free_variables = []
         # TODO refactor this block to work with complex expressions
-        dec[1] = dec[1].first == 'and' ? dec[1].drop(1) : [dec[1]]
+        dec[1] = dec[1].first == 'and' ? dec[1].drop(1) : [dec[1]] unless dec[1].empty?
         dec[1].each {|pre|
           unless pre.first == 'not' or pre.first == 'call' or axioms.assoc(pre.first) or attachments.assoc(pre.first)
             free_variables.concat(pre.select {|i| i.instance_of?(String) and i.start_with?('?') and not met[1].include?(i)})
