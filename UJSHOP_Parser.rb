f9e3cbc070b9c410f7e770322f84f2a77420cc2a
Mau Magnaguagno <maumagnaguagno@gmail.com>
2017-07-26 03:23:13 -0300

    Consider nested calls in UJSHOP_Parser define_expression

--- UJSHOP_Parser.rb
+++ UJSHOP_Parser.rb
@@ -60,9 +60,11 @@ module UJSHOP_Parser
       define_expression(name, group.last)
     elsif first == 'call'
       raise "Unexpected list as function name in #{name}" if group[1].instance_of?(Array)
+      group.drop(2).each {|g| define_expression(name, g) if g.instance_of?(Array) and g.first == 'call'}
     elsif first == 'assign'
       raise "Wrong number of arguments for #{group.join(' ')}, expected 2" if group.size != 3
       raise "Expected a variable to assign, instead of #{group[1]}" unless group[1].start_with?('?')
+      define_expression(name, group.last) if group.last.instance_of?(Array) and group.last.first == 'call'
     elsif a = @axioms.assoc(first)
       raise "Axiom #{first} defined with arity #{a[1].size}, unexpected arity #{group.size.pred}" if a[1].size != group.size.pred
     elsif a = @attachments.assoc(first)
