aaa83b5cf7610cdfd9d01908c4ff3ab1e5afb373
Mau Magnaguagno <maumagnaguagno@gmail.com>
2017-08-16 15:53:13 -0300

    Add boat example

---
+++ examples/boat/external.rb
@@ -0,0 +1,31 @@
+require_relative '../../../Polygonoid/examples/search/Search'
+
+module External
+  extend self
+
+  @symbol_object = {'start' => Point.new(0,0)}
+  @pos_counter = 0
+
+  def symbol(object)
+    symbol = @symbol_object.key(object) or @symbol_object[symbol = "pos#{@pos_counter += 1}"] = object
+    symbol
+  end
+
+  def advance(pos, angle, amount, newpos)
+    # TODO add wind, current and error in amount and angle
+    pos = @symbol_object[pos]
+    angle = angle.to_f
+    amount = amount.to_f
+    x = pos.x + amount * Math.cos(angle)
+    y = pos.y + amount * Math.sin(angle)
+    # Expected path
+    newpos.replace(symbol(Point.new(x,y)))
+    yield
+    # Overshoot error
+    amount += 1
+    x = pos.x + amount * Math.cos(angle)
+    y = pos.y + amount * Math.sin(angle)
+    newpos.replace(symbol(Point.new(x,y)))
+    yield
+  end
+end
