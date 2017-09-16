4c20537bb11d3ea1e1bca2a825fb498a79ce1655
Mau Magnaguagno <maumagnaguagno@gmail.com>
2017-09-16 03:57:48 -0300

    Rename test to distinguish search examples

--- tests/path.rb
+++ tests/path.rb
@@ -3,7 +3,7 @@ require './tests/hypest'
 class Path < Test::Unit::TestCase
   include Hypest
 
-  def test_search_pb1_ujshop_parsing
+  def test_search_angle_pb1_ujshop_parsing
     parser_tests(
       # Files
       'examples/search_angle/search.ujshop',
