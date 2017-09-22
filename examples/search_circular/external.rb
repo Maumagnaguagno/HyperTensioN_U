f08bc6c7944ca32cdb886c6b5e1ef8d8e2abfa53
Mau Magnaguagno <maumagnaguagno@gmail.com>
2017-09-22 17:45:52 -0300

    Remove unused constants and methods from search_circular external

--- examples/search_circular/external.rb
+++ examples/search_circular/external.rb
@@ -1,9 +1,6 @@
 require_relative '../../../Polygonoid/examples/search/Search'
 require_relative '../../../Polygonoid/examples/circular/Circular'
 
-CLOCK = 'clock'
-COUNTER = 'counter'
-
 module External
   extend self
   srand(0)
@@ -18,20 +15,6 @@ module External
   def symbol(object)
     symbol = @symbol_object.key(object) or @symbol_object[symbol = "pos#{@pos_counter += 1}"] = object
     symbol
-  end
-
-  def visible(from, to, a, b)
-    visible?(Line.new(@symbol_object[from], @symbol_object[to]), CIRCLES, @symbol_object[a], @symbol_object[b])
-  end
-
-  def bitangent(pos, from, to, in_dir_symbol, out_dir_symbol, out_circle)
-    each_bitangent(@symbol_object[pos], in_dir_symbol, CIRCLES) {|c,line,out_dir|
-      from.replace(symbol(line.from))
-      to.replace(symbol(line.to))
-      out_dir_symbol.replace(out_dir)
-      out_circle.replace(symbol(c))
-      yield
-    }
   end
 
   def search_circular(agent, start, goal)
