b5f65deb8b4da9eec3b5885ba97a42c697b2cb6a
Mau Magnaguagno <maumagnaguagno@gmail.com>
2018-05-27 03:46:17 -0300

    Use forwardable for external protection and function generator methods

--- examples/generator/external.rb
+++ examples/generator/external.rb
@@ -1,3 +1,4 @@
+require 'forwardable'
 require_relative '../../../Hypertension/examples/experiments/Protection'
 require_relative '../../../Hypertension/examples/experiments/Function'
 
@@ -16,38 +17,9 @@ end
 
 module External
   extend self
+  extend Forwardable
 
-  def protect(pos, neg)
-    Generator.protect(pos, neg)
-  end
-
-  def unprotect(pos, neg)
-    Generator.unprotect(pos, neg)
-  end
-
-  def function(f)
-    Generator.function(f)
-  end
-
-  def assign(f, value)
-    Generator.assign(f, value)
-  end
-
-  def increase(f, value)
-    Generator.increase(f, value)
-  end
-
-  def decrease(f, value)
-    Generator.decrease(f, value)
-  end
-
-  def scale_up(f, value)
-    Generator.scale_up(f, value)
-  end
-
-  def scale_down(f, value)
-    Generator.scale_down(f, value)
-  end
+  def_delegators Generator, :protect, :unprotect, :function, :assign, :increase, :decrease, :scale_up, :scale_down
 
   def time(t, min = 0, max = Float::INFINITY, epsilon = 1)
     min.step(max, epsilon) {|i|
