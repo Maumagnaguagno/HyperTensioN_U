7c728c0dbab6223a94bd6abb4c44f5ca2475a120
Mau Magnaguagno <maumagnaguagno@gmail.com>
2018-06-04 10:18:47 -0300

    Update Generator domain and external to match Continuous

--- examples/generator/external.rb
+++ examples/generator/external.rb
@@ -3,15 +3,13 @@ require_relative '../../../HyperTensioN/examples/experiments/Protection'
 require_relative '../../../HyperTensioN/examples/experiments/Function'
 
 module Generator
-  prepend Protection
-  include Continuous
+  prepend Protection, Continuous
 
   def problem(state, *args)
     state[:function] = {
       ['fuellevel', 'gen'] => 1000 - state['available'].size * 20,
       ['capacity', 'gen'] => 1000
     }
-    state[:continuous] = []
     super(state, *args)
   end
 
@@ -28,7 +26,7 @@ module External
   extend self
   extend Forwardable
 
-  def_delegators Generator, :protect, :unprotect, :function, :assign, :increase, :decrease, :scale_up, :scale_down, :activate
+  def_delegators Generator, :protect, :unprotect, :function, :assign, :increase, :decrease, :scale_up, :scale_down, :process, :event
 
   def time(t, min = 0, max = Float::INFINITY, epsilon = 1)
     min.step(max, epsilon) {|i|
