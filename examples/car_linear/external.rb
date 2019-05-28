3e90800551c66b77e977c14423df5137392490ad
Mau Magnaguagno <maumagnaguagno@gmail.com>
2019-05-28 13:35:28 -0300

    Remove debug features from Car Linear

--- examples/car_linear/external.rb
+++ examples/car_linear/external.rb
@@ -3,7 +3,7 @@ require_relative '../../../HyperTensioN/examples/experiments/Function'
 require_relative '../../../HyperTensioN/examples/experiments/Debug'
 
 module Car_linear
-  prepend Continuous, Debug
+  prepend Continuous
 
   # (:process displacement :precondition (engine_running) :effect (increase (d) (* #t (v))) )
   def displacement(t)
@@ -40,7 +40,7 @@ end
 module External
   extend self, Forwardable
 
-  def_delegators Car_linear, :function, :process, :event, :print_state, :input
+  def_delegators Car_linear, :function, :process, :event
 
   def step(t, min = 0.0, max = Float::INFINITY, epsilon = 1.0)
     min.to_f.step(max.to_f, epsilon.to_f) {|i|
