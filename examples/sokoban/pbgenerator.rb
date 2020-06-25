be9e1ef56466b6477a6d8e8369ca7dc2618ff15b
Mau Magnaguagno <maumagnaguagno@gmail.com>
2020-06-25 10:53:50 -0300

    Simplify Sokoban problem generator

--- examples/sokoban/pbgenerator.rb
+++ examples/sokoban/pbgenerator.rb
@@ -33,7 +33,6 @@
   x = y = 0
   player = nil
   level.each_char {|c|
-    new_line = false
     case c
     when '#' then problem << "\n    (wall p#{x}_#{y})"
     when '@' then player = "p#{x}_#{y}"
@@ -45,11 +44,10 @@
     when '.' then problem << "\n    (clear p#{x}_#{y})\n    (storage p#{x}_#{y})"
     when ' ' then problem << "\n    (clear p#{x}_#{y})"
     when "\n"
-      new_line = true
-      x = 0
+      x = -1
       y += 1
     end
-    x += 1 unless new_line
+    x += 1
   }
   problem << "\n  )\n  (\n    (solve #{player})\n  )\n)"
   IO.binwrite(File.expand_path("../pb#{i}.ujshop", __FILE__), problem)
