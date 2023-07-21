b5c3652d1394bd08a0866b30c2f2d1d43dd193cb
Mau Magnaguagno <maumagnaguagno@gmail.com>
2023-07-21 17:55:51 -0300

    Preprocess deadlock and initial state visit in Sokoban external problem

--- examples/sokoban/external.rb
+++ examples/sokoban/external.rb
@@ -2,7 +2,28 @@ module Sokoban
 
   DIRS = [[1,0],[-1,0],[0,1],[0,-1]]
 
-  @visited = Hash.new {|h,k| h[k] = false; true}
+  def problem(state, tasks, *args)
+    # Find deadlocks
+    storage = state['storage']
+    deadlocks = state['deadlock'] = []
+    map = []
+    state['wall'].each {|wall,|
+      x, y = wall.delete_prefix('p').split('_')
+      (map[y.to_i] ||= [])[x.to_i] = true
+    }
+    map[1..-2].each_with_index {|row,y|
+      y += 1
+      row[1..-2].each_with_index {|cell,x|
+        if not (cell or storage.include?(p = ["p#{x += 1}_#{y}"])) and (map[y-1][x] or map[y+1][x]) and (row[x-1] or row[x+1])
+          deadlocks << p
+        end
+      }
+    }
+    # Visit initial position
+    @visited = Hash.new {|h,k| h[k] = false; true}
+    @visited[tasks[0][1].hash ^ state['box'].sort!.hash]
+    super
+  end
 
   def adjacent(from, to)
     x, y = from.delete_prefix('p').split('_')
@@ -35,24 +56,6 @@ module Sokoban
   def boxes_stored
     storage = @state['storage']
     @state['box'].all? {|p| storage.include?(p)}
-  end
-
-  def find_deadlocks
-    storage = @state['storage']
-    @state['deadlock'] = deadlocks = []
-    map = []
-    @state['wall'].each {|wall,|
-      x, y = wall.delete_prefix('p').split('_')
-      (map[y.to_i] ||= [])[x.to_i] = true
-    }
-    map[1..-2].each_with_index {|row,y|
-      y += 1
-      row[1..-2].each_with_index {|cell,x|
-        if not (cell or storage.include?(p = ["p#{x += 1}_#{y}"])) and (map[y-1][x] or map[y+1][x]) and (row[x-1] or row[x+1])
-          deadlocks << p
-        end
-      }
-    }
   end
 
   def new_state(player)
